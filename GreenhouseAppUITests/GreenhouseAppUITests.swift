15c9e493f79b9938e67f67369c533cd9dbce8fb6
Temxter <anrgry1@gmail.com>
2019-06-05 11:33:58 +0300

    add tests

--- GreenhouseAppUITests/GreenhouseAppUITests.swift
+++ GreenhouseAppUITests/GreenhouseAppUITests.swift
@@ -26,8 +26,55 @@ class GreenhouseAppUITests: XCTestCase {
     }
 
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let login = "test"
+        let password = "123"
+        app/*@START_MENU_TOKEN@*/.buttons["Sign up"]/*[[".segmentedControls.buttons[\"Sign up\"]",".buttons[\"Sign up\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let loginField = app.textFields["Login"]
+        loginField.tap()
+        loginField.typeText(login)
+        
+        let passwordSecureTextField = app.secureTextFields["Password"]
+        passwordSecureTextField.tap()
+        passwordSecureTextField.typeText(password)
+        
+        let repeatPasswordSecureTextField = app.secureTextFields["Repeat password"]
+        repeatPasswordSecureTextField.tap()
+        repeatPasswordSecureTextField.typeText(password)
+
+       // let rulesSwitch 
+        
+        app.buttons["Enter"].tap()
+        app.statusBars.children(matching: .other).element.children(matching: .other).element(boundBy: 0).tap()
+        XCUIDevice.shared.orientation = .portrait
+        XCUIDevice.shared.orientation = .portrait
+        app.buttons["Your orders"].tap()
+        app.buttons["Order"].tap()
+        app.alerts["Make order"].buttons["OK"].tap()
+        
+        let backButton = app.buttons["Back"]
+        backButton.tap()
+        app.buttons["Our branches"].tap()
+        app.alerts["Allow “GreenhouseApp” to also access your location even when you are not using the app?"].buttons["Always Allow"]/*@START_MENU_TOKEN@*/.tap()/*[[".tap()",".press(forDuration: 1.1);"],[[[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+                app.staticTexts["Label"].tap()
+        backButton.tap()
+        app.buttons["Make an order"].tap()
+        
+        let compositionPriceCountTable = app.tables["Composition, Price, Count"]
+        compositionPriceCountTable.tap()
+        compositionPriceCountTable.tap()
+        
+        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element
+        element.children(matching: .other).element.children(matching: .other).element.tap()
+        
+        let searchCompositionSearchField = app.searchFields["Search composition"]
+        searchCompositionSearchField.tap()
+        searchCompositionSearchField.tap()
+        app.buttons["Cart"].tap()
+        backButton.tap()
+
     }
 
 }
